f98686c54d0bdb8dd680b62867930c436993edff
Francois Desrosiers <exarkuun@hotmail.com>
2021-05-11 20:44:24 -0400

    init project commit

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const App = () => (
+    <>
+        <h1>Hello World</h1>
+
+        <hr />
+    </>
+)
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.tsx',
+  target: "web",
+  mode: "development",  
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },  
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        include:[path.resolve(__dirname,'src')]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /.*\.(gif|png|jpe?g|svg)$/i,
+        use: [
+            {
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]',
+                outputPath: 'images/'
+            }
+            }
+        ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+        {
+            loader: 'file-loader',
+            options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+            }
+        }
+        ]
+      }      
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devServer: {
+    contentBase: './dist',
+  },  
+  plugins: [
+    new webpack.IgnorePlugin(/^\.\/config\.js$/),
+    new HtmlWebpackPlugin({
+        template: "./src/index.html"
+    }),
+  ]  
+};
